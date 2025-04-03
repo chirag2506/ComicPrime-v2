2fcdf247ce792fed9c95d3684a20f6485778b830
chirag2506 <chiraggupta2506@gmail.com>
2025-04-03 08:58:11 +0530

    analysis continues...

--- backend/parseComicScrapJson.py
+++ backend/parseComicScrapJson.py
@@ -3,7 +3,7 @@ import re
 
 comics = readJson("./comicsScrap.json")
 
-
+i = 0
 for month, metadata in comics.items():
 
     # # check if commics specified same as array length
@@ -12,20 +12,29 @@ for month, metadata in comics.items():
     #     print(month)
 
     # checking for vol, issue
-    regex = "Vol [0-9]+ [0-9]+"
+    regex = "Vol [0-9]+ -?'?([0-9]+|∞|½|X)"
+    
     for comic in metadata.get("comics", []):
-        occ = re.findall(regex, comic)
-        if len(occ) != 1:
-            print(comic)
+        
+        # occ = re.findall(regex, comic)
+        occ = re.compile(regex)
+        for m in occ.finditer(comic):
+            print(m.start(), "helloo", m.group())
+        # if len(occ) != 1:
+        #     i += 1
+        #     log.info("https://marvel.fandom.com/wiki/{}".format(comic.replace(" ", "_")))
+        #     log.info(month)
+        #     log.info("*"*100)
+        # else:
+        #     print(occ)
+            
 
     # # checking if vol, issue appears at end of each issue
-    # regex = "Vol [0-9]+ [0-9]+"
+    # regex = "Vol [0-9]+ -?'?([0-9]+|∞|½|X)"
+
     # for comic in metadata.get("comics", []):
     #     occ = re.findall(regex, comic)
 
-    #     if len(occ)
-    #     print(comic)
-    # break
     #     # nameComponents = comic.split(" ")
     #     # issueNumber = nameComponents[-1]
     #     # if not str(issueNumber).startswith("#"):
@@ -35,3 +44,4 @@ for month, metadata in comics.items():
     #     # found 12 occurances: 1971, Apr, Nov  2006...
 
     True
+print(i)
