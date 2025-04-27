76f6f3dc827d0cdbaf32f627570228cb7f0f3e89
chirag2506 <chiraggupta2506@gmail.com>
2025-04-27 15:44:47 +0530

    multithreaded push done and added comics from ff1 to apr 1972 in DB

--- analysis/addComicsFF1ToApr1972Multithread.py
+++ analysis/addComicsFF1ToApr1972Multithread.py
@@ -1,17 +1,28 @@
 import multiprocessing
 import multiprocessing.queues
-import time
 from utilities import *
 from schema import *
 import re
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
+from urllib.parse import quote
 from scrapPublishDate import getPublishDate
 from checkForReprint import checkReprint
 from datetime import datetime
 
+cred = credentials.Certificate("files/json/credentials.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 months = readJson("files/json/comicsScrapFF1ToApr1972.json")
+volumes = readJson("files/json/volumesFromFF1ToApr1972Clean.json")
+volumesToBeRead = volumes["ToBeRead"]
+volumesIgnore = volumes["Ignore"]
 regex = "Vol [0-9]+"
+comicsRef = db.collection("comics")
 
 def getComicDetails(month, comic, firestoreComicQueue):
     pattern = re.compile(regex)
@@ -23,20 +34,43 @@ def getComicDetails(month, comic, firestoreComicQueue):
         name = comic[ : occ[0].start()].strip()
         vol = int(comic[occ[0].start()+4 : occ[0].end()+1].strip())
         issue = comic[occ[0].end()+1 : ]
-        comicUrl = "https://marvel.fandom.com/wiki/{}".format(comic.replace(" ", "_"))
+        comicUrl = "https://marvel.fandom.com/wiki/{}".format(quote(comic.replace(" ", "_")))
         uClient = uReq(comicUrl)
         pageHtml = uClient.read()
         uClient.close()
         pageSoup = soup(pageHtml, "html.parser")
         isReprint = checkReprint(pageSoup, name, vol, issue)
         publishDate = getPublishDate(pageSoup)
-        comicDetails = Comic(coverYear=cYear, coverMonth=cMonth, issue=str(issue), releaseDate=publishDate, reprint=isReprint, title=name, 
-                             toBeRead=True, ######################### LOGIC TO BE UPDATED
-                             url=comicUrl, volume=vol)
-        firestoreComicQueue.put(comicDetails)
+        volumeName = "{} Vol {}".format(name, vol)
+        if volumeName in volumesIgnore or isReprint == True:
+            toBeRead = False
+        elif volumeName in volumesToBeRead:
+            toBeRead = True
+        else:
+            toBeRead = None
+            log.error("Not found volume: {} for issue {}".format(volumeName, issue))
+        if toBeRead is not None:
+            comicDetails = Comic(
+                coverYear=cYear, coverMonth=cMonth, issue=str(issue), releaseDate=publishDate,
+                reprint=isReprint, title=name, toBeRead=toBeRead, url=comicUrl, volume=vol
+            )
+            firestoreComicQueue.put(comicDetails)
     
 def addComicToFirestore(details: Comic):
-    print("Calling firestore comics for: {}".format(details))
+    print("Calling firestore comics for: {} Vol {} Issue {}".format(details.title, details.volume, details.issue))
+    keyName = re.sub("[^A-Za-z0-9]+", "", "{}vol{}issue{}".format(details.title, details.volume, details.issue)).lower()
+    comicRef = comicsRef.document(keyName)
+    comicRef.set({
+        "volume": details.volume,
+        "title": details.title,
+        "issue": details.issue,
+        "toBeRead": details.toBeRead,
+        "coverMonth": details.coverMonth,
+        "coverYear": details.coverYear,
+        "releaseDate": details.releaseDate,
+        "reprint": details.reprint,
+        "url": details.url
+    })
         
 
 # Worker function for processing tasks
@@ -44,33 +78,31 @@ def workerComic(taskQueue, processId, firestoreComicQueue):
     while True:
         try:
             task = taskQueue.get(timeout=300)  # wait for a task
+            print(f"Comic worker Process-{processId} processing {task}")
+            # time.sleep(random.uniform(0.5, 2))  # simulate work
+            getComicDetails(task[0], task[1], firestoreComicQueue)
         except multiprocessing.queues.Empty:
             print(f"Comic worker Process-{processId}: No more tasks, exiting.")
             break
-        print(f"Comic worker Process-{processId} processing {task}")
-        # time.sleep(random.uniform(0.5, 2))  # simulate work
-        getComicDetails(task[0], task[1], firestoreComicQueue)
-        print(f"Comic worker Process-{processId} finished {task}")
         taskQueue.task_done()
+        
 
 def workerFirestoreComic(taskQueue, processId):
     while True:
         try:
             task = taskQueue.get(timeout=300)
+            print(f"Firestore comic Process-{processId} processing {task}")
+            addComicToFirestore(task)
         except multiprocessing.queues.Empty:
             print(f"Firestore comic Process-{processId}: No more tasks, exiting.")
             break
-        print(f"Firestore comic Process-{processId} processing {task}")
-        time.sleep(3)  # simulate work
-        addComicToFirestore(task)
-        print(f"Firestore comic Process-{processId} finished {task}")
         taskQueue.task_done()
 
 if __name__ == "__main__":
     start = datetime.now()
     multiprocessing.set_start_method("spawn")
 
-    numProcesses = 4
+    numProcesses = 7
     manager = multiprocessing.Manager()
     taskQueue = manager.Queue()
     firestoreComicQueue = manager.Queue()
@@ -93,16 +125,17 @@ if __name__ == "__main__":
         p.start()
         processes.append(p)
 
-    # Wait for the queue to be empty
-    while not taskQueue.empty():
-        time.sleep(0.5)
-    while not firestoreComicQueue.empty():
-        time.sleep(0.5)
+    # # Wait for the queue to be empty
+    # while not taskQueue.empty():
+    #     time.sleep(0.5)
+    # while not firestoreComicQueue.empty():
+    #     time.sleep(0.5)
 
     # Wait for all processes to finish
+    # taskQueue.join()
+    # firestoreComicQueue.join()
     for p in processes:
-        p.join()
+        p.join()  ####### SAME AS ABOVE, BUT JOINS FOR EACH QUEUE FOR EACH LOOP
 
     print("All tasks completed.")
     print("time for multiple thread: {}".format(datetime.now() - start))
-# time for multiple thread: 0:00:21.563960
